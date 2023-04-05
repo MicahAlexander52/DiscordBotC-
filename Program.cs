42aef0d8609bf5803d2ce6ab011537b6745a6d10
MicahAlexander52 <127165971+MicahAlexander52@users.noreply.github.com>
2023-04-05 01:20:15 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Discord;
+using Discord.WebSocket;
+
+namespace DiscordBot
+{
+    public class Program
+    {
+        private DiscordSocketClient _client;
+        private string token = File.ReadAllText("token.txt");
+        static void Main(string[] args) => new Program().MainAsync().GetAwaiter().GetResult();
+        public async Task MainAsync()
+        {
+            var _config = new DiscordSocketConfig { MessageCacheSize = 100 };
+            _client = new DiscordSocketClient(_config);
+
+            await _client.LoginAsync(TokenType.Bot, token);
+            
+            await _client.StartAsync();
+
+            _client.MessageUpdated += MessageUpdated;
+            _client.Ready += () =>
+            {
+                Console.WriteLine("Bot is connected!");
+                return Task.CompletedTask;
+            };
+
+            await Task.Delay(-1);
+        }
+
+        private async Task MessageUpdated(Cacheable<IMessage, ulong> before, SocketMessage after, ISocketMessageChannel channel)
+        {
+            var message = await before.GetOrDownloadAsync();
+            Console.WriteLine($"{message} -> {after}");
+        }
+
+    }
+
+
+
+}
